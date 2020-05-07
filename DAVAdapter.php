f5bcd3153e4773413bd26e4917bb143df941680b
Michael Stilkerich <ms@mike2k.de>
2020-05-07 16:57:31 +0200

    initial commit of guzzle adapter and carddav discovery

---
+++ DAVAdapter.php
@@ -0,0 +1,228 @@
+<?php
+
+/*
+
+Plugin response object for HTTP requests:
+
+$reply =
+[
+	'success' => boolean,  // whether request was successful
+	'status'  => integer,  // HTTP status code returned by server
+	'statusmsg' => string, // Status message returned by server
+	'headers' =>           // Response headers as associative array (headernames are lowercased!)
+	[
+		'etag' => string,
+		...
+	],
+	'body'    =>           // raw body of the reply
+];
+
+ */
+
+
+/*
+Other needed features:
+  - Setting extra headers (Depth, Content-Type, charset, If-Match, If-None-Match)
+  - Debug output HTTP traffic to logfile
+*/
+abstract class DAVAdapter
+{
+	const NSDAV     = 'DAV:';
+	const NSCARDDAV = 'urn:ietf:params:xml:ns:carddav';
+
+	abstract public function init($base_uri, $username, $password, $options=array());
+
+	abstract public function propfind($uri, $body, $options=array());
+
+	abstract public function report($uri, $body, $options=array());
+
+	abstract public function get($uri, $options=array());
+
+	abstract public function delete($uri, $options=array());
+
+	abstract public function put($uri, $body, $options=array());
+
+	public function findCurrentUserPrincipal($uri)
+	{
+		$xml = $this->findProperties($uri, 'DAV:current-user-principal');
+		$result = false;
+
+		if ($xml !== false)
+		{
+			$princurl = $xml->xpath('//DAV:current-user-principal/DAV:href');
+			if (is_array($princurl) && count($princurl) > 0)
+			{
+				echo "principal URL: ". $princurl[0] . "\n";
+				$result = (string) $princurl[0];
+			}
+		}
+
+		return $result;
+	}
+
+	public function findAddressbookHome($principalUri)
+	{
+		$xml = $this->findProperties($principalUri, 'CARDDAV:addressbook-home-set');
+		$result = false;
+
+		if ($xml !== false)
+		{
+			$abookhome = $xml->xpath('//CARDDAV:addressbook-home-set/DAV:href');
+			if (is_array($abookhome) && count($abookhome) > 0)
+			{
+				echo "addressbook home: ". $abookhome[0] . "\n";
+				$result = (string) $abookhome[0];
+			}
+		}
+
+		return $result;
+	}
+
+	public function findAddressbooks($addressbookHomeUri)
+	{
+		$xml = $this->findProperties($addressbookHomeUri, [ 'DAV:resourcetype', 'DAV:displayname' ], "1");
+		$result = array();
+
+		if ($xml !== false)
+		{
+			// select the responses that have a successful (status 200) resourcetype addressbook response
+			$abooks = $xml->xpath("//DAV:response[DAV:propstat[contains(DAV:status,' 200 ')]/DAV:prop/DAV:resourcetype/CARDDAV:addressbook]");
+			if (is_array($abooks))
+			{
+				foreach($abooks as $abook)
+				{
+					self::registerNamespaces($abook);
+					$abookUri = $abook->xpath('child::DAV:href');
+					if (is_array($abookUri) && count($abookUri) > 0)
+					{
+						$abookUri = (string) $abookUri[0];
+
+						$abookName = $abook->xpath("child::DAV:propstat[contains(DAV:status,' 200 ')]/DAV:prop/DAV:displayname");
+						if (is_array($abookName) && count($abookName) > 0)
+						{
+							$abookName = (string) $abookName[0];
+						}
+						else
+						{
+							$abookName = basename($abookUri);
+							echo "Autosetting name from $abookUri to $abookName\n";
+						}
+
+						echo "Found addressbook at $abookUri named $abookName\n";
+						$result[] = [ "name" => $abookName, "uri" => $abookUri ];
+					}
+				}
+			}
+		}
+
+		return $result;
+	}
+	// $props is either a single property or an array of properties
+	// Namespace shortcuts: DAV for DAV, CARDDAV for the CardDAV namespace
+	// Common properties:
+	// DAV:current-user-principal
+	// DAV:resourcetype
+	// DAV:displayname
+	// CARDDAV:addressbook-home-set
+	public function findProperties($uri, $props, $depth="0")
+	{
+		if (!is_array($props))
+		{
+			$props = array($props);
+		}
+		$body  = '<?xml version="1.0" encoding="utf-8"?>' . "\n";
+		$body .= '<DAV:propfind xmlns:DAV="DAV:" xmlns:CARDDAV="urn:ietf:params:xml:ns:carddav"><DAV:prop>' . "\n";
+		foreach ($props as $prop)
+		{
+			$body .= "<" . $prop . "/>\n";
+		}
+		$body .= '</DAV:prop></DAV:propfind>';
+
+		$reply = $this->propfind($uri, $body, ["headers" => ["Depth"=>$depth ]]);
+		$xml   = self::checkAndParseXML($reply);
+		return $xml;
+	}
+
+	// XML helpers
+	public static function checkAndParseXML($davReply)
+	{
+		$xml = false;
+		if($davReply["success"] &&
+			self::check_contenttype($davReply['headers']['content-type'], ';(text|application)/xml;'))
+		{
+			$xml = self::parseXML($davReply['body']);
+		}
+
+		return $xml;
+	}
+
+	private static function parseXML($xmlString)
+	{
+		try
+		{
+			$xml = new SimpleXMLElement($xmlString);
+			self::registerNamespaces($xml);
+		}
+		catch (Exception $e)
+		{
+			echo "XML could not be parsed: " . $e->getMessage() . "\n";
+			$xml = false;
+		}
+
+		return $xml;
+	}
+
+	private static function registerNamespaces($xml)
+	{
+		$xml->registerXPathNamespace('CARDDAV', self::NSCARDDAV);
+		$xml->registerXPathNamespace('DAV', self::NSDAV);
+	}
+
+	private static function check_contenttype($ctheader, $expectedct)
+	{
+		$contentTypeMatches = false;
+
+		if(!is_array($ctheader))
+		{
+			$ctheader = array($ctheader);
+		}
+
+		foreach($ctheader as $ct)
+		{
+			if(preg_match($expectedct, $ct))
+			{
+				$contentTypeMatches = true;
+				break;
+			}
+		}
+
+		return $contentTypeMatches;
+	}
+
+	protected static function propfindMinimalOptions($options)
+	{
+		if (!array_key_exists("headers", $options))
+		{
+			$options["headers"] = array();
+		}
+
+		$hasDepthHeader = false;
+		foreach ($options["headers"] as $name => $values)
+		{
+			if (strcasecmp($name, "Depth") == 0)
+			{
+				$hasDepthHeader = true;
+				break;
+			}
+		}
+
+		if (!$hasDepthHeader)
+		{
+			$options["headers"]["Depth"] = "0";
+		}
+
+		return $options;
+	}
+}
+
+?>
---
+++ DAVAdapterGuzzle.php
@@ -0,0 +1,140 @@
+<?php
+
+require 'vendor/autoload.php';
+
+require 'DAVAdapter.php';
+
+use GuzzleHttp\Client;
+
+class DAVAdapterGuzzle extends DAVAdapter
+{
+	private $client;
+
+	private $debughandle = false;
+
+	public function __destruct()
+	{
+		if ($this->debughandle !== false)
+		{
+			echo "Closing Debug Handle\n";
+			fclose($this->debughandle);
+		}
+	}
+
+	# Options: default options for request
+	#   - headers => array('headername' => val (string) OR array(val1, val2, ...))
+	#   - debugfile => string (filename)
+	public function init($base_uri, $username, $password, $options=array())
+	{
+		$guzzleOptions = self::prepareGuzzleOptions($options);
+
+		if (array_key_exists("debugfile", $options))
+		{
+			$this->debughandle = fopen($options["debugfile"], "a");
+			$guzzleOptions["debug"] = $this->debughandle;
+		}
+
+		$guzzleOptions['http_errors'] = false; // no exceptions on 4xx/5xx status
+		$guzzleOptions['base_uri'] = $base_uri;
+		$guzzleOptions['auth'] = [$username, $password];
+
+		$this->client = new Client($guzzleOptions);
+	}
+
+	private function convertGuzzleToInternal($guzzleResponse)
+	{
+		$reply = array();
+		$statuscode = $guzzleResponse->getStatusCode();
+		$reply['success'] = ($statuscode >= 200) && ($statuscode < 300);
+		$reply['status'] = $statuscode;
+		$reply['statusmsg'] = $guzzleResponse->getReasonPhrase();
+		$reply['headers'] = array();
+		foreach ($guzzleResponse->getHeaders() as $name => $values)
+		{
+			$reply['headers'][strtolower($name)] = $values;
+		}
+		$reply['body'] = $guzzleResponse->getBody()->getContents();
+
+		if ($this->debughandle !== false)
+		{
+			fwrite($this->debughandle, $reply['body']);
+		}
+		return $reply;
+	}
+
+	private static function prepareGuzzleOptions($options)
+	{
+		$guzzleOptions = array();
+
+		if ( array_key_exists("headers", $options) )
+		{
+			$guzzleOptions["headers"] = $options["headers"];
+		}
+
+		if (array_key_exists("allow_redirects", $options) && $options["allow_redirects"] === false)
+		{
+			$guzzleOptions["allow_redirects"] = false;
+		}
+		else
+		{
+			$guzzleOptions["allow_redirects"] =
+			[
+				'max'             => 5,
+				'strict'          => true, // keep original request method, i.e. do not perform GET on redirection target
+				'referer'         => false,
+				'protocols'       => ['http', 'https'],
+				'track_redirects' => false
+			];
+		}
+
+		return $guzzleOptions;
+	}
+
+	# Options: default options for request
+	#   - headers => array('headername' => val (string) OR array(val1, val2, ...))
+	public function propfind($uri, $body, $options=array())
+	{
+		$options = parent::propfindMinimalOptions($options);
+		$guzzleOptions = self::prepareGuzzleOptions($options);
+		$guzzleOptions['body'] = $body;
+
+		$response = $this->client->request('PROPFIND', $uri, $guzzleOptions);
+		return $this->convertGuzzleToInternal($response);
+	}
+
+	public function report($uri, $body, $options=array())
+	{
+		$guzzleOptions = self::prepareGuzzleOptions($options);
+		$guzzleOptions['body'] = $body;
+
+		$response = $this->client->request('REPORT', $uri, $guzzleOptions);
+		return $this->convertGuzzleToInternal($response);
+	}
+
+	public function get($uri, $options=array())
+	{
+		$guzzleOptions = self::prepareGuzzleOptions($options);
+
+		$response = $this->client->get($uri, $guzzleOptions);
+		return $this->convertGuzzleToInternal($response);
+	}
+
+	public function delete($uri, $options=array())
+	{
+		$guzzleOptions = self::prepareGuzzleOptions($options);
+
+		$response = $this->client->delete($uri, $guzzleOptions);
+		return $this->convertGuzzleToInternal($response);
+	}
+
+	public function put($uri, $body, $options=array())
+	{
+		$guzzleOptions = self::prepareGuzzleOptions($options);
+		$guzzleOptions['body'] = $body;
+
+		$response = $this->client->put($uri, $guzzleOptions);
+		return $this->convertGuzzleToInternal($response);
+	}
+}
+
+?>

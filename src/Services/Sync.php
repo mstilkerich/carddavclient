a9ed1407feaff1e99fb1cea175d0e4601c427b57
Michael Stilkerich <ms@mike2k.de>
2020-06-14 17:04:58 +0200

    add copyright headers

--- src/Services/Sync.php
+++ src/Services/Sync.php
@@ -1,5 +1,26 @@
 <?php
 
+/*
+ * CardDAV client library for PHP ("PHP-CardDavClient").
+ *
+ * Copyright (C) 2020 Michael Stilkerich <ms@mike2k.de>
+ *
+ * This file is part of PHP-CardDavClient.
+ *
+ * PHP-CardDavClient is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * PHP-CardDavClient is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with PHP-CardDavClient.  If not, see <https://www.gnu.org/licenses/>. 
+ */
+
 /**
  * Class Sync
  */
--- src/Services/SyncHandler.php
+++ src/Services/SyncHandler.php
@@ -1,15 +1,32 @@
 <?php
 
+/*
+ * CardDAV client library for PHP ("PHP-CardDavClient").
+ *
+ * Copyright (C) 2020 Michael Stilkerich <ms@mike2k.de>
+ *
+ * This file is part of PHP-CardDavClient.
+ *
+ * PHP-CardDavClient is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * PHP-CardDavClient is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with PHP-CardDavClient.  If not, see <https://www.gnu.org/licenses/>. 
+ */
+
 /**
  * Interface for application-level synchronization handler.
  *
  * During an addressbook synchronization, the corresponding methods of this interface
  * are invoked for events such as changed or deleted address objects, to be handled
  * in an application-specific manner.
- *
- * @author Michael Stilkerich <michael@stilkerich.eu>
- * @copyright 2020 Michael Stilkerich
- * @license http://opensource.org/licenses/gpl-license.php GNU General Public License, version 2 (or later)
  */
 
 declare(strict_types=1);

4dd003a0a77dcade0a330b1c54780238afa9b54f
Michael Stilkerich <ms@mike2k.de>
2020-06-23 12:30:17 +0200

    Quickstart sample script

--- README.md
+++ README.md
@@ -8,10 +8,13 @@ This is a library for PHP applications to interact with addressbooks stored on C
 - Synchronization of the server-side addressbook and a local cache
   - Using efficient sync-collection ([RFC 6578](https://tools.ietf.org/html/rfc6578)) and addressbook-multiget ([RFC 6352](https://tools.ietf.org/html/rfc6352)) reports if supported by server
   - Falling back to synchronization via PROPFIND and comparison against the local cache state if the server does not support these reports
-- Modification of addressbooks (adding/changing/deleting address objects)  
+- Modification of addressbooks (adding/changing/deleting address objects)
 - Uses [Guzzle](https://github.com/guzzle/guzzle) HTTP client library, including support for HTTP/2 and various authentication schemes
   - OAuth/OAuth2 is *not* supported at the moment
 - Uses [Sabre/VObject](https://github.com/sabre-io/vobject) at the application-side interface to exchange VCards
+- Uses any PSR-3 compliant logger object to record log messages and the HTTP traffic. A separate logger object is used
+  for the HTTP traffic, which tends to be verbose and therefore logging for HTTP could be done to a separate location or
+  disabled independent of the library's own log messages.
 
 ## Tested Servers
 
@@ -21,22 +24,74 @@ Currently, this library has been tested to interoperate with:
 * iCloud
 * Google Contacts via CardDAV API (using HTTP Basic Authentication, which is deprecated and will be disabled by Google in the future for OAuth2)
 
+In theory, it should work with any CardDAV server.
+
 ## Installation
 
 This library is intended to be used with [composer](https://getcomposer.org/) to install/update the library and its dependencies.
 It is intended to be used with a PSR-4 compliant autoloader (as provided by composer).
 
-To use the library in your application with composer, simply load composer's autoloader:
+To add the library as a dependency to your project via composer:
+
+1. Download composer (skip if you already have composer): [Instructions](https://getcomposer.org/download/)
+
+2. Add this library as a dependency to your project
+```sh
+php composer.phar require mstilkerich/carddavclient:dev-master
+```
+
+3. To use the library in your application with composer, simply load composer's autoloader in your main php file:
 ```php
 require 'vendor/autoload.php';
 ```
+The autoloader will take care of loading this and other PSR-0/PSR-4 autoloader-compliant libraries.
 
 ## Documentation
 
-Documentation is currently only available inside the php files.
+### Quickstart
 
-### Sample Application
+Generally, an application using this library will want to do some or all of the following things:
 
-For a simple demo application that makes use of this library, see [davshell](https://github.com/mstilkerich/davshell/). It shows how to use the library for the discovery and synchronization of addressbooks and is currently the best available quick start information.
+1. Discover addressbooks from the information provided by a user: For this operation, the library provides a service
+   class *MStilkerich\CardDavClient\Services\Discovery*.
 
-You can also take a look at my fork of the [Roundcube CardDAV](https://github.com/mstilkerich/rcmcarddav) plugin, which also uses this library for the interaction with the CardDAV server.
+The service takes the account credentials and a partial URI (at the minimum a domain name) and with that attempts to
+discover the user's addressbooks. It returns an array of *MStilkerich\CardDavClient\AddressbookCollection* objects, each
+representing an addressbook.
+
+2. Recreate addressbooks in known locations, discovered earlier. This is possible by simply creating instances of
+   *MStilkerich\CardDavClient\AddressbookCollection*.
+
+3. Initially and periodically synchronize the server-side addressbook with a local cache: For this operation, the
+   library provides a service class *MStilkerich\CardDavClient\Services\Sync*.
+
+This service performs synchronization given *MStilkerich\CardDavClient\AddressbookCollection* object and optionally a
+synchronization token returned by the previous sync operation. A synchronization token is a server-side identification
+of the state of the addressbook at a certain time. When a synchronization token is given, the server will be asked to
+only report the delta between the state identified by the synchronization token and the current state. This may not work
+for various reasons, the most common being that synchronization tokens are not kept indefinitly by the server. In such
+cases, a full synchronization will be performed. At the end of the sync, the service returns the synchronization token
+reflecting the synchronized state of the addressbook, if provided by the server.
+
+3. Perform changes to the server-side addressbook such as creating new address objects. These operations are directly
+   provided as methods of the *MStilkerich\CardDavClient\AddressbookCollection* class.
+
+There is a demo script [doc/quickstart.php] distributed with the library that shows how to perform all the above
+operations.
+
+### Sample Applications
+
+For a simple demo application that makes use of this library, see [davshell](https://github.com/mstilkerich/davshell/).
+It shows how to use the library for the discovery and synchronization of addressbooks and is currently the best
+available quick start information.
+
+You can also take a look at my fork of the [Roundcube CardDAV](https://github.com/mstilkerich/rcmcarddav) plugin, which
+also uses this library for the interaction with the CardDAV server.
+
+### API documentation
+
+Documentation for the API can be generated from the source code using [phpDocumentor](https://www.phpdoc.org/).
+
+```sh
+phpdoc -d src/ -t doc/api/ --title="CardDAV Client Library"
+```
---
+++ doc/quickstart.php
@@ -0,0 +1,189 @@
+<?php
+
+///////////////////////////////////////
+//   CONFIGURATION OF ACCOUNT DATA   //
+///////////////////////////////////////
+
+// To run, configure the account data in this block and remove / comment out
+// the following line with the exit statement
+// DO NOT SERVE THIS SCRIPT VIA WEBSERVER
+echo "Please configure first - exiting\n"; exit(1);
+
+const DISCOVERY_URI = "example.com";
+const USERNAME = "myUserName@example.com";
+
+// Note: for providers using 2-factor authentication (common today, e.g. Apple iCloud,
+// Google, Nextcloud if 2FA enabled by user), you need to provide an application-specific
+// password here, not your account password. You can typically create such
+// application-specific passwords in the account settings of your provider.
+const PASSWORD = "theSecretPassword";
+
+///////////////////////////////////////
+// END CONFIGURATION OF ACCOUNT DATA //
+///////////////////////////////////////
+
+require 'vendor/autoload.php';
+
+use MStilkerich\CardDavClient\{Account, AddressbookCollection, Config};
+use MStilkerich\CardDavClient\Services\{Discovery, Sync, SyncHandler};
+
+use Psr\Log\{AbstractLogger, NullLogger, LogLevel};
+use Sabre\VObject\Component\VCard;
+
+// This is just a sample logger for demo purposes. You can use any PSR-3 compliant logger,
+// there are many implementations available (e.g. monolog)
+class StdoutLogger extends AbstractLogger
+{
+    public function log($level, $message, array $context = array())
+    {
+        if ($level !== LogLevel::DEBUG) {
+            $ctx = empty($context) ? "" : json_encode($context);
+            echo $message . $ctx . "\n";
+        }
+    }
+}
+
+class EchoSyncHandler implements SyncHandler
+{
+	public function addressObjectChanged(string $uri, string $etag, VCard $card): void
+	{
+		echo "   +++ Changed or new card $uri (ETag $etag): " . $card->FN . "\n";
+	}
+
+    public function addressObjectDeleted(string $uri): void
+	{
+		echo "   --- Deleted Card $uri\n";
+	}
+
+	public function getExistingVCardETags(): array
+	{
+		return [];
+	}
+
+	public function finalizeSync(): void
+	{
+	}
+}
+
+$log = new StdoutLogger();
+$httplog = new NullLogger(); // parameter could simply be omitted for the same effect
+
+// Initialize the library. Currently, only objects for logging need to be provided, which are two optional logger
+// objects implementing the PSR-3 logger interface. The first object logs the log messages of the library itself, the
+// second can be used to log the HTTP traffic. If no logger is given, no log output will be created. For that, simply
+// call Config::init() and the library will internally use NullLogger objects.
+Config::init($log, $httplog);
+
+// Now create an Account object that contains credentials and discovery information
+$account = new Account(DISCOVERY_URI, USERNAME, PASSWORD);
+
+// Discover the addressbooks for that account
+try {
+	$log->notice("Attempting discovery of addressbooks");
+
+	$discover = new Discovery();
+	$abooks = $discover->discoverAddressbooks($account);
+} catch (\Exception $e) {
+	$log->error("!!! Error during addressbook discovery: " . $e->getMessage());
+	exit(1);
+}
+
+$log->notice(">>> " . count($abooks) . " addressbooks discovered");
+foreach ($abooks as $abook) {
+	$log->info(">>> - $abook");
+}
+
+if (count($abooks) <= 0) {
+	$log->warning("Cannot proceed because no addressbooks were found - exiting");
+	exit(0);
+}
+//////////////////////////////////////////////////////////
+// THE FOLLOWING SHOWS HOW TO PERFORM A SYNCHRONIZATION //
+//////////////////////////////////////////////////////////
+$abook = $abooks[0];
+$synchandler = new EchoSyncHandler();
+$syncmgr = new Sync();
+
+// initial sync - we don't have a sync-token yet
+$log->notice("Performing initial sync");
+$lastSyncToken = $syncmgr->synchronize($abook, $synchandler, [ "FN" ], "");
+$log->notice(">>> Initial Sync completed, new sync token is $lastSyncToken");
+
+// every subsequent sync would be passed the sync-token returned by the previous sync
+// there most certainly won't be any changes to the preceding one at this point and we
+// can expect the same sync-token be returned again
+$log->notice("Performing followup sync");
+$lastSyncToken = $syncmgr->synchronize($abook, $synchandler, [ "FN" ], $lastSyncToken);
+$log->notice(">>> Re-Sync completed, new sync token is $lastSyncToken");
+
+//////////////////////////////////////////////////////////////
+// THE FOLLOWING SHOWS HOW TO PERFORM CHANGES ON THE SERVER //
+//////////////////////////////////////////////////////////////
+
+
+// First, we want to insert a new card, so we create a fresh one
+// See https://sabre.io/vobject/vcard/ on how to work with Sabre VCards
+// CardDAV VCards require a UID property, which the carddavclient library will
+// generate and insert automatically upon storing a new card lacking this property
+
+try {
+	$vcard =  new VCard([
+		'FN'  => 'John Doe',
+		'N'   => ['Doe', 'John', '', '', ''],
+	]);
+
+
+	$log->notice("Attempting to create a new card on the server");
+	[ 'uri' => $cardUri, 'etag' => $cardETag ] = $abook->createCard($vcard);
+	$log->notice(">>> New card created at $cardUri with ETag $cardETag");
+
+	// now a sync should return that card as well - lets see!
+	$log->notice("Performing followup sync");
+	$lastSyncToken = $syncmgr->synchronize($abook, $synchandler, [ "FN" ], $lastSyncToken);
+	$log->notice(">>> Re-Sync completed, new sync token is $lastSyncToken");
+
+	// add an EMAIL address to the card and update the card on the server
+	$vcard->add(
+		'EMAIL',
+		'johndoe@example.org',
+		[
+			'type' => ['home'],
+			'pref' => 1,
+		]
+	);
+
+	// we pass the ETag of our local copy of the card to updateCard. This
+	// will make the update operation fail if the card has changed on the
+	// server since we fetched our local copy
+	$log->notice("Attempting to update the previously created card at $cardUri");
+	$cardETag = $abook->updateCard($cardUri, $vcard, $cardETag);
+	$log->notice(">>> Card updated, new ETag: $cardETag");
+
+	// again, a sync should report that the card was updated
+	$log->notice("Performing followup sync");
+	$lastSyncToken = $syncmgr->synchronize($abook, $synchandler, [ "FN" ], $lastSyncToken);
+	$log->notice(">>> Re-Sync completed, new sync token is $lastSyncToken");
+
+	// finally, delete the card
+	$log->notice("Deleting card at $cardUri");
+	$abook->deleteCard($cardUri);
+	// now, the sync should report the card was deleted
+	$log->notice("Performing followup sync");
+	$lastSyncToken = $syncmgr->synchronize($abook, $synchandler, [ "FN" ], $lastSyncToken);
+	$log->notice(">>> Re-Sync completed, new sync token is $lastSyncToken");
+
+	$log->notice("All done, good bye");
+} catch (\Exception $e) {
+	$log->error("Error while making changes to the addressbook: " . $e->getMessage());
+	$log->error("Manual cleanup (deletion of the John Doe card) may be needed");
+
+	// do one final attempt to delete the card
+	try {
+		if (isset($cardUri)) {
+			$abook->deleteCard($cardUri);
+		}
+	} catch (\Exception $e) {
+	}
+
+	exit(1);
+}

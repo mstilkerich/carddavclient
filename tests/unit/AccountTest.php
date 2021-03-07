bb2cdc71282f1e4145fb284ba8190bf57ac40ff3
Michael Stilkerich <ms@mike2k.de>
2021-03-07 09:51:28 +0100

    Test for Account serialization/deserialization

--- tests/unit/AccountTest.php
+++ tests/unit/AccountTest.php
@@ -34,6 +34,72 @@ final class AccountTest extends TestCase
         $this->expectException(\Exception::class);
         $account->getUrl();
     }
+
+    public function testAccountToStringContainsEssentialData(): void
+    {
+        $account = new Account("example.com", "theUser", "thePassword", "https://carddav.example.com:443");
+        $s = (string) $account;
+
+        $this->assertStringContainsString("example.com", $s);
+        $this->assertStringContainsString("user: theUser", $s);
+        $this->assertStringContainsString("CardDAV URI: https://carddav.example.com:443", $s);
+    }
+
+    public function testCanBeCreatedFromArray(): void
+    {
+        $account = new Account("example.com", "theUser", "thePassword", "https://carddav.example.com:443");
+
+        $accSerial = [
+            'username' => 'theUser',
+            'password' => 'thePassword',
+            'baseUrl' => 'https://carddav.example.com:443',
+            'discoveryUri' => 'example.com'
+        ];
+        $accountExp = Account::constructFromArray($accSerial);
+
+        $this->assertEquals($accountExp, $account);
+    }
+
+    public function testCanBeCreatedFromArrayWithoutOptionalProps(): void
+    {
+        $account = new Account("example.com", "theUser", "thePassword");
+
+        $accSerial = [
+            'username' => 'theUser',
+            'password' => 'thePassword',
+            'discoveryUri' => 'example.com'
+        ];
+        $accountExp = Account::constructFromArray($accSerial);
+
+        $this->assertEquals($accountExp, $account);
+    }
+
+    public function testCanBeCreatedFromArrayWithoutRequiredProps(): void
+    {
+        $accSerial = [
+            'password' => 'thePassword',
+            'baseUrl' => 'https://carddav.example.com:443',
+            'discoveryUri' => 'example.com'
+        ];
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage("does not contain required property username");
+        Account::constructFromArray($accSerial);
+    }
+
+    public function testCanBeSerializedToJson(): void
+    {
+        $account = new Account("example.com", "theUser", "thePassword", "https://carddav.example.com:443");
+
+        $accSerial = $account->jsonSerialize();
+        $accSerialExp = [
+            'username' => 'theUser',
+            'password' => 'thePassword',
+            'baseUrl' => 'https://carddav.example.com:443',
+            'discoveryUri' => 'example.com'
+        ];
+
+        $this->assertEquals($accSerialExp, $accSerial);
+    }
 }
 
 // vim: ts=4:sw=4:expandtab:fenc=utf8:ff=unix:tw=120
